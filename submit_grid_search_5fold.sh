2c9c540fbe19702121950edcfa0f259b14bb0087
rbonazzola <rodbonazzola@gmail.com>
2025-07-02 13:19:12 +0100

    Added 5-fold CV script

---
+++ submit_grid_search_5fold.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+seeds=(42)
+n_embd_vals=(240)
+n_layer_vals=(6)
+batch_size_vals=(128)
+learning_rate_vals=(1e-4)
+val_folds=(1 2 3 4 5)
+
+for seed in "${seeds[@]}"; do
+  for embd in "${n_embd_vals[@]}"; do
+    for layer in "${n_layer_vals[@]}"; do
+      for batch in "${batch_size_vals[@]}"; do
+        for lr in "${learning_rate_vals[@]}"; do
+          for val_fold in "${val_folds[@]}"; do
+            sbatch train.slurm --val_fold $val_fold --n_embd=$embd --n_layer=$layer --batch_size=$batch --learning_rate=$lr --seed=$seed --min_lr=$(awk "BEGIN {print $lr / 10}")
+            sleep 5
+          done
+        done
+      done
+    done
+  done
+done
