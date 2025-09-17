0c554eb7d36ccf0c999eac25800a90b80fdb0f66
rbonazzola <rodbonazzola@gmail.com>
2025-09-17 15:17:21 +0100

    Brought util scripts from main branch

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,14 @@ import pandas as pd
 import torch
 import re
 import os
+import ast 
 
+PADDING_TOKEN = 0
+NO_EVENT_TOKEN_ID = 1
+MASKING_TOKEN, MASKING_AGE = -1, -10000
+LIFESTYLE_MIN_INDEX, LIFESTYLE_MAX_INDEX = 3, 11
+
+is_lifestyle_token = lambda tokens: (tokens >= LIFESTYLE_MIN_INDEX) * (tokens <= LIFESTYLE_MAX_INDEX)
 
 def get_p2i(data):
     patient_ids = data[:, 0].astype(int)
@@ -38,21 +45,13 @@ def get_batch(ix, data, p2i, select='center', index='patient', padding='regular'
         b: target ages
     """
 
-    MASKING_TOKEN, MASKING_AGE = -1, -10000
-    # LIFESTYLE_MIN_INDEX, LIFESTYLE_MAX_INDEX = 3+359, 11+359
-    LIFESTYLE_MIN_INDEX, LIFESTYLE_MAX_INDEX = 3, 11
-
     # Define the columns of the data array    
-    SUBJECT_ID_COLUMN = 0
-    AGE_COLUMN = 1
-    TOKEN_COLUMN = 2
+    SUBJECT_ID_COLUMN, AGE_COLUMN, TOKEN_COLUMN= 0, 1, 2
 
     subject_start_and_count = torch.tensor(np.array([p2i[int(i)] for i in ix]))
     if return_subject_ids:
         subject_ids = torch.tensor(np.array([data[int(subject_index[0]), SUBJECT_ID_COLUMN] for subject_index in subject_start_and_count]))        
-        
     ix = torch.tensor(np.array(ix))
-
     gen = torch.Generator(device='cpu')
     gen.manual_seed(ix.sum().item())  # we want some things be random, but also deterministic
 
@@ -75,14 +74,16 @@ def get_batch(ix, data, p2i, select='center', index='patient', padding='regular'
     batch_idx = np.arange(block_size + 1)[None, :] + traj_start_idx[:, None]
 
     mask = torch.from_numpy(data[:, SUBJECT_ID_COLUMN][batch_idx].astype(np.int64))
-    mask = mask == torch.tensor(data[p2i[ix.numpy()][:, SUBJECT_ID_COLUMN], SUBJECT_ID_COLUMN][:, None].astype(np.int64)).to(mask.dtype)
+    mask = mask == torch.tensor(
+        data[p2i[ix.numpy()][:, SUBJECT_ID_COLUMN], SUBJECT_ID_COLUMN][:, None].astype(np.int64)
+    ).to(mask.dtype)
 
     tokens = torch.from_numpy(data[:, TOKEN_COLUMN][batch_idx].astype(np.int64))
     ages   = torch.from_numpy(data[:, AGE_COLUMN][batch_idx].astype(np.float32))
 
     # augment lifestyle tokens to avoid immortality bias
     if lifestyle_augmentations:
-        lifestyle_idx = (tokens >= LIFESTYLE_MIN_INDEX) * (tokens <= LIFESTYLE_MAX_INDEX)
+        lifestyle_idx = is_lifestyle_token(tokens) # (tokens >= LIFESTYLE_MIN_INDEX) * (tokens <= LIFESTYLE_MAX_INDEX)
         n_lifestyles_tokens = lifestyle_idx.sum()
         if n_lifestyles_tokens:
             ages[lifestyle_idx] += torch.randint(-20*365, 365*40, (n_lifestyles_tokens,), generator=gen).float()
@@ -90,7 +91,7 @@ def get_batch(ix, data, p2i, select='center', index='patient', padding='regular'
     tokens = tokens.masked_fill(~mask, MASKING_TOKEN)
     ages   = ages.masked_fill(~mask, MASKING_AGE)
 
-    # insert a "no event" token every 5 years on average
+    # insert a "no event" token every "no_event_token_rate" years on average
     if (padding.lower() == 'none' or
             padding is None or
             no_event_token_rate == 0 or
@@ -184,6 +185,7 @@ def get_person(idx):
         person.append((id_to_token[token_id.item()], date.item()))
     return person, y, time[0][-1]
 
+
 class DelphiData:
     def __init__(self, data_dir, val_fold, delphi_labels, labels, device=None):
         self.data_dir = data_dir
@@ -209,9 +211,6 @@ class DelphiData:
         
         self.train_p2i = get_p2i(self.train_data)
         self.val_p2i = get_p2i(self.val_data)
-        
-        self.all_data = np.concatenate([self.train_data, self.val_data])
-        self.all_p2i  = get_p2i(self.all_data) 
 
     def get_id_to_token(self):
         self.id_to_token = self.labels.to_dict()[0]
@@ -275,3 +274,62 @@ class DelphiData:
         tokens = [i[0] for i in p]
         ages = [i[1] for i in p]
         return tokens, ages
+    
+
+def get_val_data(val_filename):
+    val_data = np.memmap(val_filename, dtype=np.int32).reshape(-1, 3)
+    val_p2i = get_p2i(val_data)
+    return val_data, val_p2i
+
+
+def fix_artifact_uri(artifact_uri):
+    artifact_uri = re.sub(pattern="^file://", repl="", string=artifact_uri)
+    artifact_uri = re.sub(pattern=".*/mlruns", repl="mlruns", string=artifact_uri)
+    import pathlib
+    artifact_uri = pathlib.Path(artifact_uri)
+    return artifact_uri
+
+
+def get_epoch_from_ckpt(ckpt_path):
+    return int(ckpt_path.split("_")[-1].split(".")[0])
+
+
+def get_ignored_tokens(runinfo, validation_loss_mode = True):
+    """
+    Get the list of ignored tokens from the runinfo.
+    """
+    ignored_tokens = ast.literal_eval(runinfo['ignore_tokens'])
+    if validation_loss_mode:
+        ignored_tokens += [NO_EVENT_TOKEN_ID]    
+    
+    if isinstance(ignored_tokens, int):
+        ignored_tokens = [ignored_tokens]
+    return ignored_tokens
+
+
+def get_top_counts(data, labels, top_n=200, ignored_tokens=[]):
+
+    id_to_token = dict(zip(labels.index-1, labels.name))
+
+    counts = pd.DataFrame(data, columns=["subject_id", "age", "token_id"]).\
+        query("token_id not in @ignored_tokens").\
+        assign(token=lambda df: df.token_id.apply(lambda x: id_to_token[x])).\
+        token.value_counts(ascending=False).\
+        head(top_n).\
+        sort_values()
+    
+    return counts
+
+
+def get_wte(model):
+   wte = model.transformer.wte.weight.detach().numpy()
+   return pd.DataFrame(wte, index=[ id_to_token[i] for i in range(-1, len(id_to_token)-1) ])
+
+
+def get_best_ckpt(runinfo):
+    """
+    Get the path to the best checkpoint from the runinfo.
+    """
+    ckpt_dir = fix_artifact_uri(runinfo.artifact_uri) / "checkpoints"
+    best_ckpt_path = ckpt_dir / sorted(os.listdir(ckpt_dir), key=get_epoch_from_ckpt)[-1]
+    return best_ckpt_path
