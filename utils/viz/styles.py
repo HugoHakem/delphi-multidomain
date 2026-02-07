ceba20e85ba48350e5352bb1dcff36fb2c54ba5b
rbonazzola <rodbonazzola@gmail.com>
2026-02-07 15:27:54 +0000

    Added utils for trajectory visualization

---
+++ utils/viz/styles.py
@@ -0,0 +1,13 @@
+
+
+def color_by_domain(row):
+
+    colors = {
+        'hla_alleles': 'background-color: #A0E5E5',
+        'sex': 'background-color: #E5F5FF',
+        'lifestyle': 'background-color: #B0B0E5',
+        'diseases': 'background-color: #FFF5E5',
+        'death': 'background-color: #F5E5FF',
+        'padding': 'background-color: #F0F0F0',
+    }
+    return [colors.get(row['domain_name'], '')] * len(row)
