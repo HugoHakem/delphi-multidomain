29aec5299325e944392861f89554536cf1d4f70b
rbonazzola <rodbonazzola@gmail.com>
2026-02-09 20:51:32 +0000

    Removed unnecessary auxiliary functions

--- utils/cv_utils.py
+++ utils/cv_utils.py
@@ -3,60 +3,6 @@ import re
 import os
 from typing import List, Dict
 
-def get_best_ckpt_from_mlflow(runid):
-    runinfo = mlflow.get_run(run_id=runid)
-    mlflow_uri = os.path.dirname(mlflow.get_tracking_uri()) + "/"
-    ckpt_dir = re.sub(r'^.*(?=mlruns)', mlflow_uri, runinfo.info.artifact_uri) + '/checkpoints'
-    best_ckpt = [x for x in os.listdir(ckpt_dir) if 'best' in x][0]
-    best_ckpt = os.path.join(ckpt_dir, best_ckpt)
-    return best_ckpt
-
-
-# Deprecated but may contain useful elements
-def get_run_from_fold(experiment_id=None, val_fold=None):
-    """ 
-    Retrieves val_fold and the checkpoint path from MLflow metadata using experiment_id.
-    Ensures the experiment contains all 5 folds.
-    Returns (val_fold, run_id, ckpt_path).
-    """
-
-    experiment_id = str(experiment_id)
-
-    client = mlflow.tracking.MlflowClient()
-    runs = client.search_runs(
-        experiment_ids=[experiment_id],
-        filter_string="attributes.status = 'FINISHED'",
-        order_by=["attributes.start_time DESC"],
-        max_results=1000
-    )   
-    if not runs:
-        raise ValueError(f"No successful runs found in experiment {experiment_id}")
-
-    val_folds = []
-    fold_to_run = {}
-    for run in runs:
-        val_fold_run = None
-        if 'fold' in run.data.params:
-            val_fold_run = int(run.data.params['fold'])
-        elif 'fold' in run.data.tags:
-            val_fold_run = int(run.data.tags['fold'])
-        if val_fold_run is not None:
-            if val_fold_run not in fold_to_run:
-                fold_to_run[val_fold_run] = run 
-                val_folds.append(val_fold_run)
-
-    if sorted(val_folds) != [1, 2, 3, 4, 5]: 
-        raise ValueError(f"Experiment {experiment_id} does not contain all 5 folds! Found folds: {sorted(val_folds)}")
-
-    requested_fold = val_fold
-
-    if requested_fold not in fold_to_run:
-        raise ValueError(f"Requested fold {requested_fold} not found in experiment {experiment_id}")
-
-    run = fold_to_run[requested_fold]
-
-    return run.info.run_id
-
 
 def load_fold_ids(fold_dir: str, num_folds: int = 10) -> List[List[str]]:
     """
